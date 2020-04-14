a11ccd14b03565b508de4a175255d05152c0286a
Mehul <aroram8@stanford.edu>
2020-04-13 19:51:44 -0700

    updated p1

--- dieroll.py
+++ dieroll.py
@@ -10,19 +10,23 @@ numtrials = 100000
 success = 0
 
 for i in range(numtrials):
-	outcomes = set()
+	outcomes = []
+	numtrios = 0
 
 	# Generate 6 die throw results
 	for roll in range(6): 
 		outcome = np.random.randint(1, 7)
-		outcomes.add(outcome)
+		outcomes.append(outcome)
 
 	# Set will have length 4 iff only one number was repeated 3 times. 
-	if len(outcomes) == 4:
+	for num in range(6):
+		if outcomes.count(num) == 3:
+			numtrios += 1
+
+	if numtrios == 1:
 		success += 1
 
 	if (i + 1) % 10000 == 0:
 		print("{} trials done".format(i + 1))
-		print(outcomes)
 
 print(success / numtrials)
